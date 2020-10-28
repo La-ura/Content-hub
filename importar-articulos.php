a5ba7a17fe1143dc0855ef027570e7d3ebe7592d
Laura Ramirez <laura.ramirez@televisatim.com>
2020-10-28 13:38:03 -0600

    Nuevos archvivos

---
+++ importar-articulos.php
@@ -0,0 +1,162 @@
+<?
+/*
+Plugin Name: Importador de artículos del API de televisa
+Plugin URI: https://hubcontent.televisa.xyz/api
+Description: Importa notas de las agencias :Europress , Notimex ,EFE, Xinhua ,AFP , AP ,DPA , APTN ,Reuters 
+Version:1
+Author:Laura Ramírez
+License: GPL
+*/
+// in the main plugin file
+define( 'MYPLUGIN_FILE', __FILE__ );
+register_activation_hook(MYPLUGIN_FILE, 'insert_category' );
+add_action('admin_menu', 'add_botton_import_article' );
+/* Guarda en wp el articulo */
+add_action('wp_ajax_nopriv_import_note_from_api','api_save_cont');
+add_action('wp_ajax_import_note_from_api','api_save_cont');
+/* Limpia campos de caracteres no validos */
+add_action('wp_ajax_nopriv_clean_data','clean_fields');
+add_action('wp_ajax_clean_data','clean_fields');
+
+function add_botton_import_article(){
+    add_menu_page("Importar Articulos de agencias","Content Hub",'manage_options','importar-articulos', 'get_articles' ,"dashicons-align-none");
+   
+}
+function clean_field($str){
+  $healthy = array("<headline>" , "</headline>" , "^V^V^Aa^_-r i^S^Q");
+  $newphrase = str_replace($healthy, "", $str);
+  return $newphrase ;
+}
+function api_save_cont(){
+  $postArray = $_POST["datos"];
+  $category =0 ;
+  $title = clean_field($postArray["title"]);
+  $conte = clean_field($postArray["content"]);
+  $category = $postArray["category"];
+  $idcategory = get_category_id($category);
+
+  $my_post = array(
+    'post_title'    =>  $title ,
+    'post_content'  =>   $conte ,
+    'post_author'   => get_current_user_id(),
+    'post_category' => array( $idcategory )
+  );
+  $id = wp_insert_post( $my_post );
+  $url = get_edit_post_link($id , "");
+  echo  $url ;
+  //echo "Id" . $id  . " title : ". $title ." body : ".$conte;
+  wp_die();
+}
+
+function get_category_id($category_json){
+  $parent = get_term_by( 'name', 'Agencia', 'category');
+  $term = term_exists( $category_json,  'category', $parent->term_id);
+  return $term['term_id'];
+}
+
+function insert_category(){
+  $term = term_exists( 'agencias',  'category' );
+   if( $term === NULL ){
+       wp_insert_term('Agencias',  'category', 
+       array('slug' => 'agencias',    ));
+ 
+       wp_insert_term(  'Deportes',   'category',
+         array( 'slug' => 'deportes',  'parent'=> term_exists( 'Agencias', 'category' )['term_id'] ));
+ 
+       wp_insert_term( 'Internacional', 'category',
+          array('slug' => 'Internacional',  'parent'=> term_exists( 'Agencias', 'category' )['term_id']  ));
+     
+       wp_insert_term( 'Tecnología', 'category',
+       array('slug' => 'tecnologia',  'parent'=> term_exists( 'Agencias', 'category' )['term_id']  ));
+ 
+       wp_insert_term( 'Ambiente', 'category',
+       array('slug' => 'ambiente',  'parent'=> term_exists( 'Agencias', 'category' )['term_id']  )); 
+      
+       wp_insert_term( 'Entretenimiento', 'category',
+       array('slug' => 'entretenimiento',  'parent'=> term_exists( 'Agencias', 'category' )['term_id']  ));  
+       
+       wp_insert_term( 'Ciencia', 'category',
+       array('slug' => 'ciencia',  'parent'=> term_exists( 'Agencias', 'category' )['term_id']  ));    
+ 
+       wp_insert_term( 'Salud', 'category',
+       array('slug' => 'salud',  'parent'=> term_exists( 'Agencias', 'category' )['term_id']  ));    
+   }
+  }
+
+function get_articles(){
+    wp_enqueue_script("jquery");
+    wp_enqueue_script("jquery-ui-datepicker");
+    wp_enqueue_script("jquery-ui-accordion");
+    wp_enqueue_script("jquery-ui-selectmenu");
+    wp_enqueue_script("jquery-ui-button");
+    wp_enqueue_script("jquery-ui-dialog");
+    wp_enqueue_script("import-article-api" ,  plugin_dir_url( __FILE__ ) . 'js/import-article-api.js');
+    //wp_register_style('note-jquery-ui-style', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/themes/blitzer/jquery-ui.css', false, null);
+    //  wp_enqueue_style('note-jquery-ui-style');
+    wp_register_style('note-jquery-ui-style',   plugin_dir_url( __FILE__ ) . 'css/jquery-ui-blitzer.css' );
+    wp_enqueue_style('note-jquery-ui-style');
+    wp_register_style('note-api-styles',  plugin_dir_url( __FILE__ ) . 'css/style.css' );
+    wp_enqueue_style('note-api-styles');
+
+   ?>
+<div id="wrap">
+<h1>Content Hub</h1>
+<div id="lb_content_row1">
+  <div class ="tb1">T&iacute;tulo   </div>
+  <div class ="tb2">Fecha Inicio</div>
+  <div class ="tb3">Fecha Final</div>
+  <div class ="tb4">Categor&iacute;a</div>
+  <div class ="tb5"> Agencias</div>
+  <div class ="tb6"> Items</div>
+</div>
+  <div id="lb_content_row2">
+      <div id="lb_cont_title"><input type="text" id="note_title" placeholder="Buscar por t&iacute;tulo" ></div>
+      <div id="lb_date1">  <input type="text" id="date_start" placeholder=" yyy/mm/dd"></div>
+      <div id="lb_date2"> <input type="text" id="date_end" placeholder=" yyy/mm/dd"></div>
+      <div id="lb_category">
+        <select name="note_category" id="note_category">
+              <option value=""> Selecciona una categor&iacute;a</option>
+              <option value="Ambiente">Ambiente</option>
+              <option value="Ciencia">Ciencia</option>
+              <option value="Deportes">Deportes</option>
+              <option value="Entretenimiento">Entretenimiento</option>
+              <option value="Internacional">Internacional</option>
+              <option value="Salud">Salud</option>
+              <option value="Tecnologia">Tecnolog&iacute;a</option>
+            </select>
+      </div>
+      <div id="lb_agencie">
+        <select name="note_agencie" id="note_agencie">
+              <option value="">Selecciona una agencia</option>
+              <option value="afp">AFP</option>
+              <option value="ap">AP</option>
+              <option value="APTN">APTN</option>
+              <option value="DPA">DPA</option>
+              <option value="EFE">EFE</option>
+              <option value="Europress">EUROPRESS</option>
+              <option value="Notimex">NOTIMEX</option>
+              <option value="Reuters">REUTERS</option>
+              <option value="Xinhua">XINHUA</option>
+        </select> 
+      </div>
+
+      <div id="lb_itmes"> 
+      <select name="note_items" id="note_items">
+              <option value="20">20</option>
+              <option value="40">40</option>
+              <option value="60">60</option>
+              <option value="80">80</option>
+              <option value="100">100</option>
+      </select>
+      </div>
+  </div>
+ <div id="lb_cont_btn"> <button id ="search_note" class="ui-button ui-widget ui-corner-all" onclick="show_article()" >Buscar </button></div>
+    <div id="dialog-confirm" style ="display:none; " title="Importar artículo">
+  <div><span class="" style="float:left; margin:12px 12px 20px 0;"></span>¿Está seguro de importar este artículo?</div>
+</div>
+    <div id="content-list-notes"></div>
+</div>
+</div>
+   <?
+
+}
---
+++ js/import-article-api.js
@@ -0,0 +1,171 @@
+var jq2 = jQuery.noConflict();
+jq2(function( $ ) {
+  jq2("#date_start" ).datepicker(
+    { dateFormat: 'yy-mm-dd' }
+  ).datepicker("setDate",'now');
+  jq2("#date_end" ).datepicker(
+    { dateFormat: 'yy-mm-dd' }
+  ).datepicker("setDate",'now');
+  
+  jq2("#search_note").button();
+   // $("#note_agencie").selectmenu();
+   // $("#note_category").selectmenu();
+   show_article();
+});
+
+function confirm_save_note(id){
+  jq2( "#dialog-confirm" ).dialog({
+      resizable: false,
+      height: "auto",
+      width: 400,
+      modal: false,
+      buttons: {
+        Yes: function() {
+          save_note(id)
+          jq2( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          jq2( this ).dialog( "close" );
+        }
+      }
+    });
+}
+ function save_note(id){
+  let data = {}
+  data.title = jq2(`#ap_title_${id}`).html();
+  data.source = jq2(`#source_${id}`).val();
+  data.date = jq2(`#date_${id}`).val();
+  data.category =jq2(`#category_${id}`).val();
+  data.content =jq2(`#conten_${id}`).html();
+  jq2.ajax({
+			url : ajaxurl,
+			type: 'post',
+			data: {
+				action : 'import_note_from_api',
+				datos: data
+      } ,
+      beforeSend: function(){
+        jq2(`#loading_${id}`).show();
+        jq2(`#loading_${id}`).html(" Por favor espere .... <img src = './images/loading.gif' >");
+			},
+    success: function(resultado){
+      jq2(`#loading_${id}`).hide();
+      jq2(`#loading_${id}`).html("");
+      window.open(resultado, '_blank' );
+      //console.log(resultado);
+      //jq2(location).attr('href',resultado).attr('target','_blank');
+    }
+ });
+}
+
+async function search_notes(){
+   let date_start =  jq2("#date_start").val();
+   let date_end = jq2("#date_end").val();
+   let note_title = jq2("#note_title").val();
+   let note_category = jq2("#note_category").val();
+   let note_agencie = jq2("#note_agencie").val();
+   let note_items = jq2("#note_items").val();
+   jq2("#content-list-notes").html(``);  
+   let filter = {};
+  if(date_start != ""){
+    filter.inputDateS = date_start;
+      if(date_end != ""){
+        filter.inputDateE = date_end;
+      }else{
+        alert("Indique el rango de fecha");
+        jq2("#date_end").focus();
+       
+      }
+   }
+
+   if(note_items != ""){
+    filter.items = parseInt (note_items);
+    }
+   if(note_title != ""){
+     filter.inputTitle = note_title;
+   }
+   if(note_category != ""){
+     filter.inputCategory = note_category;
+   }
+   if(note_agencie != ""){
+    filter.inputSource= note_agencie;
+   }
+
+console.log(filter);
+    let urlApi = 'https://hubcontent.televisa.xyz/api';
+    //let urlApi = 'http://localhost:3000/api';
+    let title = "covid 19";
+    let query = `query buscaArticle($inputTitle: String , $inputCategory : String, $inputDateS :  Date , $inputDateE : Date ,$items : Int,  $inputSource : String) {
+    searchArticles(inputTitle :$inputTitle, inputCategory : $inputCategory , inputDateS : $inputDateS , inputDateE : $inputDateE ,items : $items, inputSource : $inputSource ){
+      _id
+      title
+      category
+      publishDate
+      source
+      content
+    }
+}`;
+
+let opc = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables:  filter 
+  })
+}
+
+let response = await fetch(urlApi, opc)
+let json = await response.json()
+let article = json.data.searchArticles
+return article
+}
+
+function show_article(){
+  search_notes().then(article => { 
+    let totalElem = article.length;
+  if(totalElem>0){
+      jq2("#content-list-notes").html(`<div id="accordion"></div>`);  
+      let num =1;
+    for(let i in article){
+        jq2("#accordion").append(`<h3>
+              <div class="hd_note">
+                  <a href = "#" onClick = "confirm_save_note(${i});"> &nbsp;&nbsp; | &nbsp;&nbsp; IMPORTAR  &nbsp;&nbsp; | </a>
+                  &nbsp;&nbsp;  ${article[i].publishDate} &nbsp;&nbsp;| &nbsp;&nbsp;${article[i].category} &nbsp;&nbsp;| &nbsp;&nbsp;${article[i].source} 
+                </div> 
+            <div class="lb_list_num">${num}.- </div> <div  id="ap_title_${i}"> ${article[i].title}</div>
+              </h3>
+              <div><div class ="lavelLoading" id="loading_${i}"></div>
+                  <div id="conten_${i}" > ${article[i].content}</div>
+              <input type = "hidden" id ="category_${i}" value = "${article[i].category}" >   
+              <input type = "hidden" id ="source_${i}" value = "${article[i].source}" > 
+              <input type = "hidden" id ="date_${i}" value = "${article[i].publishDate}" >   
+              <input type = "hidden" id ="id_monog_${i}" value = "${article[i]._id}" >   
+              </div>`);
+        num++;
+      }
+      jq2("#accordion").accordion( { heightStyle: "content" });
+  }else {
+    jq2("#content-list-notes").html(`<div id="noElements">NO SE ENCONTRARON ELEMENTOS CON ESTOS CRITERIOS :(</div>`);  
+  }
+});
+}
+/*
+function clean_data(article){
+  
+  console.log("dentro de clean");
+        jq2.ajax({
+              url : ajaxurl,
+              type: 'post',
+              data: {
+                action : 'clean_data',
+                datos: article
+              },
+            success: function(resultado){
+              console.log(resultado);
+            }
+        });
+}*/
